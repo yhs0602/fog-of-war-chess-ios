1e942c9dee2607edf0bb82a0c60045d346d1f116
KYHSGeekCode <jourhyang123@gmail.com>
2023-08-08 15:29:43 +0900

    Initial Commit

---
+++ fogofwarchess/fogofwarchessApp.swift
@@ -0,0 +1,17 @@
+//
+//  fogofwarchessApp.swift
+//  fogofwarchess
+//
+//  Created by 양현서 on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct fogofwarchessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
